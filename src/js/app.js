703804653a68cd230d862e8a4c559d5c6f654903
VladimsKov <vladimskv@yandex.ru>
2022-09-15 00:07:15 +0300

    work added

--- src/js/app.js
+++ src/js/app.js
@@ -4,22 +4,25 @@ import read from './reader';
 import json from './parser';
 import GameSaving from './gameSavings';
 // import GameSavingLoader from './gameSavingLoader';
-/* 
+
 class GameSavingLoader {
-  static load() {
-    return read()
-      .then((data) => json(data))
-      .then((data) => {
-        const res = JSON.parse(data);
-        return new GameSaving(
-          res.id,
-          res.created,
-          res.userInfo,
-        );
-      });
+  static async load() {
+    const data = await read();
+    const outputData = await json(data);
+    const res = JSON.parse(outputData);
+    return new GameSaving(
+      res.id,
+      res.created,
+      res.userInfo,
+    );
   }
 }
- */
-/* GameSavingLoader.load()
-  .then((saving) => saving,
-    (error) => error); */
+
+(async () => {
+  try {
+    const data = await GameSavingLoader.load();
+    return data;
+  } catch (err) {
+    return err.name;
+  }
+})();
--- src/js/gameSavingLoader.js
+++ src/js/gameSavingLoader.js
@@ -4,15 +4,15 @@ import GameSaving from './gameSavings';
 
 export default class GameSavingLoader {
   static load() {
-    return read()
-      .then((data) => json(data))
-      .then((data) => {
-        const res = JSON.parse(data);
-        return new GameSaving(
-          res.id,
-          res.created,
-          res.userInfo,
-        );
-      });
+    (async () => {
+      const data = await read();
+      const jsonData = await json(data);
+      const outputData = JSON.parse(jsonData);
+      return new GameSaving(
+        outputData.id,
+        outputData.created,
+        outputData.userInfo,
+      );
+    })();
   }
 }
