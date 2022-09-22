573da71969efda67480ff87fb2ea536a389fae2f
VladimsKov <vladimskv@yandex.ru>
2022-09-22 08:17:54 +0300

    corrected

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,6 @@
 // TODO: write your code here
 
-import read from './reader';
-import json from './parser';
-import GameSaving from './gameSavings';
-// import GameSavingLoader from './gameSavingLoader';
-
-class GameSavingLoader {
-  static async load() {
-    const data = await read();
-    const outputData = await json(data);
-    const res = JSON.parse(outputData);
-    return new GameSaving(
-      res.id,
-      res.created,
-      res.userInfo,
-    );
-  }
-}
+import GameSavingLoader from './gameSavingLoader';
 
 (async () => {
   try {
--- src/js/gameSavingLoader.js
+++ src/js/gameSavingLoader.js
@@ -3,16 +3,14 @@ import json from './parser';
 import GameSaving from './gameSavings';
 
 export default class GameSavingLoader {
-  static load() {
-    (async () => {
-      const data = await read();
-      const jsonData = await json(data);
-      const outputData = JSON.parse(jsonData);
-      return new GameSaving(
-        outputData.id,
-        outputData.created,
-        outputData.userInfo,
-      );
-    })();
+  static async load() {
+    const data = await read();
+    const outputData = await json(data);
+    const res = JSON.parse(outputData);
+    return new GameSaving(
+      res.id,
+      res.created,
+      res.userInfo,
+    );
   }
 }
